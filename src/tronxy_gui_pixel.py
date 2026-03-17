bf69179c1a5ee9aa20b6d79f64dbf3c810f597d3
Guillaume Tritz-Guden <guillaumetritzguden@icloud.com>
2026-03-17 13:35:09 +0000

    Some fixes

--- src/tronxy_gui_pixel.py
+++ src/tronxy_gui_pixel.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox, ttk
 import threading
-from tronxy_control import TronxyController
+from .tronxy_control import TronxyController
 
 
 class TronxyPixelGUI:
